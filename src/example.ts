c53ad368dbaddace1053d87f505b22af45989392
Crystal <inkloud3@gmail.com>
2022-04-14 22:30:35 +0200

    msg

--- src/example.ts
+++ src/example.ts
@@ -1,3 +1,168 @@
+
 export const add = (a: number, b: number): number => {
     return a + b
+}
+
+const characters: Character[] = [] ;
+
+function maps(char: Character): void {
+    characters.push(char)
+    if (characters.length >1) {
+        for (let i = 0 ; i< characters.length;i++){
+            //random distance beetween 1 and 10
+            const distance: number = Math.floor(Math.random() * 10) + 1
+            characters[i].setDistance(char, distance)
+            char.setDistance(characters[i],distance)
+        }    
+    }
+}
+
+export class Things {
+
+    private status: string;
+
+    constructor(private name: string, private health: number){
+        this.name = name;
+        this.health = health
+        this.status = "Alive"
+    }
+
+    public getName(): string {
+        return this.name
+    }
+    
+    public setHealth(value: number) :void {
+        this.health = value;
+        if(this.health <= 0){
+            this.status = "Destroyed"
+        }
+    }
+
+    public getHealth() : number {
+        return this.health
+    }
+
+    public isAlive(): string {
+        return this.status
+    }
+
+}
+
+export class Character {
+
+    private health: number;
+    private level: number;
+    private alive: boolean;
+    //array di distanza di questo personaggio con gli altri personaggi creati
+    private distance: Map<Character,number>;
+    private faction: string;
+
+    constructor(private range: number){
+        this.health = 1000;
+        this.level= 1;
+        this.alive= true;
+        this.range = range
+        this.distance = new Map<Character,number>()
+        this.faction = ""
+        maps(this)
+    }
+
+    public getHealth(): number{
+        return this.health
+    }
+
+    public getFaction(): string {
+        return this.faction
+    }
+
+    public joinFaction(name: string): void {
+        this.faction = name;
+    }
+
+    public leaveFaction(): void {
+        this.faction = ""
+    }
+
+    public getDistance(char: Character): number {
+        const distance = this.distance.get(char)
+        if(distance == undefined){
+            throw new Error ("char undefined")
+        } else {
+            return distance
+        }
+    }
+
+    public setDistance(char: Character, metres: number): void {
+        this.distance.set(char, metres)
+    }
+
+    public getLevel(): number {
+        return this.level;
+    }
+
+    public getAlive(): boolean {
+        return this.alive;
+    }
+
+    public getRange(): number {
+        return this.range
+    }
+
+    public setLevel(value: number): void {
+        this.level = value
+    }
+
+    public addHealth(value: number): void {
+        if(this.alive){
+            this.health += value
+            if(this.health > 1000) {
+                this.health = 1000
+            }
+        }
+    }
+
+    public decreaseHealth(value : number): void {
+        this.health -= value
+        if(this.health <= 0) {
+            this.alive = false;
+            this.health = 0;
+        }
+    }
+
+    public damage(char: Character|Things, value: number): void {
+        if(char!=this && (char instanceof Character)){
+            if(this.getFaction() != char.getFaction() || char.getFaction()== ""){
+                const levels = char.getLevel() - this.getLevel()
+                if(this.getDistance(char) < this.range){
+                    if(levels >= 5) {
+                        char.decreaseHealth(value/2);
+                    } else if (levels <= -5){
+                        char.decreaseHealth(value+value*0.5);
+                    } else {
+                        char.decreaseHealth(value);
+                    }
+                }
+            }
+        }else if(char instanceof Things){
+            char.setHealth(char.getHealth() - value)
+        }   
+    }
+
+    public heal(char: Character, value: number): void {
+        if(char == this || (char.getFaction() == this.getFaction() && char.getFaction() != "")) {
+            char.addHealth(value)
+        }
+    }
+}
+
+export class Melee extends Character {
+    public constructor() {
+        super(2);
+    }
+}
+
+export class Ranged extends Character {
+    public constructor() {
+        super(20);
+    }
 }
--- tests/example.test.ts
+++ tests/example.test.ts
@@ -1,7 +1,117 @@
-import { add } from '../src/example'
+import { add, Character, Melee, Ranged, Things } from '../src/example'
 
 describe('Example Test:', () => {
     test('should add two numbers', () => {
         expect(add(1, 2)).toBe(3)
+    })
+})
+
+describe('iteration one test:', () => {
+
+    const char1 = new Character(100);
+    const char2 = new Character(100);
+    const char3 = new Character(100);
+
+    test('create character test', () => {
+        expect(char1.getHealth()).toBe(1000);
+        expect(char1.getLevel()).toBe(1);
+        expect(char1.getAlive()).toBe(true)
+    })
+
+    test('damage ', () => {
+        char2.damage(char1,500);
+        expect(char1.getHealth()).toBe(500);
+        char2.damage(char1,500);
+        expect(char1.getHealth()).toBe(0);
+        expect(char1.getAlive()).toBe(false);
+        //char1 is dead
+    })
+
+    test('heal', () => {
+        char2.damage(char3,500);
+        char3.heal(char3,200);
+        expect(char3.getHealth()).toBe(700)
+        char3.heal(char3,500);
+        expect(char3.getHealth()).toBe(1000)
+        char1.heal(char1,500);
+        expect(char1.getHealth()).toBe(0)
+    })
+
+})
+
+describe ('iteration two test: ', () => {
+    const char1 = new Character(100)
+    const char2 = new Character(100)
+
+    test('A Character cannot Deal Damage to itself', () => {
+        char1.damage(char1,400)
+        expect(char1.getHealth()).toBe(1000)
+    })
+
+    test('A Character can only Heal itself', () => {
+        char1.damage(char2,400)
+        char1.heal(char2,200)
+        expect(char2.getHealth()).toBe(600)
+        char2.heal(char2,200)
+        expect(char2.getHealth()).toBe(800)
+    })
+
+    test('If the target is 5 o more', () => {
+        char1.setLevel(6)
+        char2.damage(char1,200)
+        expect(char1.getHealth()).toBe(900)
+        char1.damage(char2,200)
+        expect(char2.getHealth()).toBe(500)
+    })
+
+})
+
+describe ('iteration three', () => {
+    test('Ranged attack', () => {
+       const melee = new Melee()
+       const ranged = new Ranged()
+       melee.damage(ranged, 100)
+       if(melee.getDistance(ranged) < melee.getRange()) {
+            expect(ranged.getHealth()).toBe(900)
+       } else{
+           expect(ranged.getHealth()).toBe(1000)
+       }
+    })
+})
+
+describe ('iteration four: factions test', () => {
+        const char1 = new Ranged()
+        const char2 = new Ranged()
+        const char3 = new Ranged()
+    test('Characters may belong to one or more Factions', () => {
+        expect(char1.getFaction()).toBe("")
+        char1.joinFaction("Hello")
+        expect(char1.getFaction()).toBe("Hello")
+        char1.leaveFaction()
+        expect(char1.getFaction()).toBe("")
+    })
+
+    test('Allies cannot Deal Damage to one another', () => {
+        char1.joinFaction("Hello")
+        char2.joinFaction("Hello")
+        char1.damage(char2, 200)
+        expect(char2.getHealth()).toBe(1000)
+    })
+
+    test('Allies can Heal one another', () => {
+        char3.damage(char1, 500)
+        char2.heal(char1,200)
+        expect(char1.getHealth()).toBe(700)
+    })
+})
+
+describe('Iteration Five ', () => {
+    const char1 = new Ranged()
+    const tree = new Things("Tree",2000)
+
+    test('thigs test', () => {
+        char1.damage(tree, 2000)
+        expect(tree.getHealth()).toBe(0)
+        expect(tree.isAlive()).toBe("Destroyed")
     })
 })
